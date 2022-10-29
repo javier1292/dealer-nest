35e436ad84ed92cc41ef96dd014ac08da4512b22
Javier Luciano <javierluciano12345@gmail.com>
2022-10-29 15:42:50 -0400

    firat commit

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+constructor(
+  private readonly carscervice: CarsService
+){}
+  @Get()
+  getallcars() {
+    return this.carscervice.findAll();
+  }
+
+  @Get(':id')
+  getCarsById(@Param('id', ParseIntPipe ) id:number) {
+    console.log({ id });
+    return this.carscervice.findByid(id);
+  } 
+}
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { json } from 'node:stream/consumers';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'toyota',
+            model: 'corolla'
+        },
+        {
+            id: 2,
+            brand: 'honda',
+            model: 'crv'
+        },
+        {
+            id: 3,
+            brand: 'jeep',
+            model: 'cherokee'
+        }
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+    findByid(id: number) {
+        const car = this.cars.find(car => car.id === id)
+        
+        return car;
+    }
+
+}
