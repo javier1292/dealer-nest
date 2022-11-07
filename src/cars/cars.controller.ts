3aea60f0e203d33e9f8e4860e4e28519ecfa8ce8
javier1292 <66648525+javier1292@users.noreply.github.com>
2022-11-07 14:58:16 -0400

    last commit

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,39 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
-
+import { CreateCarDTO } from './DTO/create-car.dto';
+import { UpdateCarDTO } from './DTO/update-car.dto';
 @Controller('cars')
 export class CarsController {
 
-constructor(
-  private readonly carscervice: CarsService
-){}
+  constructor(
+    private readonly carscervice: CarsService
+  ) { }
   @Get()
   getallcars() {
     return this.carscervice.findAll();
   }
 
   @Get(':id')
-  getCarsById(@Param('id', ParseIntPipe ) id:number) {
+  getCarsById(@Param('id', ParseUUIDPipe) id: string) {
     return this.carscervice.findByid(id);
-  } 
+  }
+
+  @Post()
+  create(@Body() createCarDto: CreateCarDTO) {
+    return this.carscervice.crete(createCarDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDTO
+  ) {
+
+    return this.carscervice.update(id, updateCarDto);
+
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carscervice.delete(id);
+  }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,22 +1,25 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, Delete } from '@nestjs/common';
 import { json } from 'node:stream/consumers';
-
+import { Car } from '../cars/interfaces/cars.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDTO, UpdateCarDTO } from './DTO/index';
+import { error } from 'node:console';
 @Injectable()
 export class CarsService {
 
-    private cars = [
+    private cars: Car[] = [
         {
-            id: 1,
+            id: uuid(),
             brand: 'toyota',
             model: 'corolla'
         },
         {
-            id: 2,
+            id: uuid(),
             brand: 'honda',
             model: 'crv'
         },
         {
-            id: 3,
+            id: uuid(),
             brand: 'jeep',
             model: 'cherokee'
         }
@@ -25,9 +28,42 @@ export class CarsService {
     findAll() {
         return this.cars;
     }
-    findByid(id: number) {
+
+    crete(createCarDto: CreateCarDTO) {
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
+        this.cars.push(car)
+        return car;
+    }
+    update(id: string, updatecarDto: UpdateCarDTO) {
+        let cardB = this.findByid(id);
+
+        if (updatecarDto.id && updatecarDto.id !== id)
+            throw new BadRequestException('Car id is not valid ')
+
+        this.cars = this.cars.map(car => {
+            if (car.id === id) {
+                cardB = {
+                    ...cardB,
+                    ...updatecarDto,
+                    id
+                }
+                return cardB;
+            }
+            return car;
+        })
+        return cardB;
+    }
+
+    delete(id:string){
+        const cardB = this.findByid(id);
+        this.cars = this.cars.filter(car => car.id !== id );
+    }
+    findByid(id: string) {
         const car = this.cars.find(car => car.id === id);
-        if(!car){
+        if (!car) {
             throw new NotFoundException(`Carro no encontrado`);
         }
         return car;
